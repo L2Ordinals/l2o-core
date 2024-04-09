38f2d1b72517aa6d5eb1d1df9f293f90f55091e5
GopherJ <alex_cj96@foxmail.com>
2024-04-09 19:33:47 +0800

    chore: cleanup
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- l2o_indexer_ordhook/src/lib.rs
+++ l2o_indexer_ordhook/src/lib.rs
@@ -42,7 +42,6 @@ use l2o_store_rocksdb::KVQRocksDBStore;
 use serde::Deserialize;
 use serde::Serialize;
 use tokio::net::TcpListener;
-use tokio::net::TcpStream;
 use tokio::sync::Mutex;
 
 type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
@@ -50,39 +49,7 @@ type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
 pub mod l2o;
 pub mod proof;
 pub mod store;
-static INDEX: &[u8] = b"<a href=\"test.html\">test.html</a>";
-static INTERNAL_SERVER_ERROR: &[u8] = b"Internal Server Error";
 static NOTFOUND: &[u8] = b"Not Found";
-static POST_DATA: &str = r#"{"original": "data"}"#;
-static URL: &str = "http://127.0.0.1:3000/json_api";
-
-async fn client_request_response() -> anyhow::Result<Response<BoxBody>> {
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri(URL)
-        .header(header::CONTENT_TYPE, "application/json")
-        .body(Full::new(Bytes::from(POST_DATA)))
-        .unwrap();
-
-    let host = req.uri().host().expect("uri has no host");
-    let port = req.uri().port_u16().expect("uri has no port");
-    let stream = TcpStream::connect(format!("{}:{}", host, port)).await?;
-    let io = TokioIo::new(stream);
-
-    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
-
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            tracing::error!("Connection error: {:?}", err);
-        }
-    });
-
-    let web_res = sender.send_request(req).await?;
-
-    let res_body = web_res.into_body().boxed();
-
-    Ok(Response::new(res_body))
-}
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct BitcoinTransactionDataV2 {
@@ -382,30 +349,12 @@ async fn api_post_response(
     Ok(response)
 }
 
-async fn api_get_response() -> anyhow::Result<Response<BoxBody>> {
-    let data = vec!["foo", "bar"];
-    let res = match serde_json::to_string(&data) {
-        Ok(json) => Response::builder()
-            .header(header::CONTENT_TYPE, "application/json")
-            .body(full(json))
-            .unwrap(),
-        Err(_) => Response::builder()
-            .status(StatusCode::INTERNAL_SERVER_ERROR)
-            .body(full(INTERNAL_SERVER_ERROR))
-            .unwrap(),
-    };
-    Ok(res)
-}
-
 async fn route(
     store: Arc<Mutex<L2OStoreV1Core<KVQRocksDBStore>>>,
     req: Request<IncomingBody>,
 ) -> anyhow::Result<Response<BoxBody>> {
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") | (&Method::GET, "/index.html") => Ok(Response::new(full(INDEX))),
-        (&Method::GET, "/test.html") => client_request_response().await,
         (&Method::POST, "/api/events") => api_post_response(store, req).await,
-        (&Method::GET, "/json_api") => api_get_response().await,
         _ => {
             // Return 404 not found response.
             Ok(Response::builder()
